d61ad1e359ad1324cd848ade3a91885cecd2aa3d
Oleg Grigorov <golantster@gmail.com>
2019-02-18 11:37:46 +0200

    update

--- scripts/openstack-helm-10.sh
+++ scripts/openstack-helm-10.sh
@@ -6,3 +6,7 @@ git clone https://git.openstack.org/openstack/openstack-helm.git
 
 apt install build-essential -y
 cd ./openstack-helm && ./tools/deployment/developer/common/010-deploy-k8s.sh
+
+./tools/deployment/developer/common/020-setup-client.sh
+
+./tools/deployment/developer/common/030-ingress.sh
