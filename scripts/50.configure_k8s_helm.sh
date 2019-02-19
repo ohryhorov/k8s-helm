b33e110f8b341308277ae04cc3b645f15686b817
Oleg Grigorov <golantster@gmail.com>
2019-02-19 22:01:03 +0200

    update

--- scripts/50.configure_k8s_helm.sh
+++ scripts/50.configure_k8s_helm.sh
@@ -5,7 +5,7 @@
 kubeadm init --pod-network-cidr=10.20.0.0/16
 
 mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
