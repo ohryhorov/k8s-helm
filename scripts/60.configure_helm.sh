1a58a100d704e275aece15c7dccb372c9548607d
Oleg Grigorov <golantster@gmail.com>
2019-02-20 13:06:11 +0200

    update

--- scripts/60.configure_helm.sh
+++ scripts/60.configure_helm.sh
@@ -4,17 +4,17 @@ kubectl get pods --all-namespaces --kubeconfig /root/.kube/config
 
 date
 
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml --kubeconfig /root/.kube/config
 
 #curl -L https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml -o S{TEMPLATE_DIR}/calico.yaml
 #5. Define CIDR in calico.yaml
 #6. kubectl apply -f ./calico.yaml
-kubectl get pods --all-namespaces
+kubectl get pods --all-namespaces --kubeconfig /root/.kube/config
 
-kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig /root/.kube/config
 
 wget https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-linux-amd64.tar.gz && tar zxvf helm-v2.12.3-linux-amd64.tar.gz && mv ./linux-amd64/helm /usr/bin
 
-kubectl create serviceaccount -n kube-system tiller
-kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller
+kubectl create serviceaccount -n kube-system tiller --kubeconfig /root/.kube/config
+kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller --kubeconfig /root/.kube/config
 helm init --service-account tiller
