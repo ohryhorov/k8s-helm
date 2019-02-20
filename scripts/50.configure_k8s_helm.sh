689aa0cdafd9e51b24349a5a4b7633cd0e654016
Oleg Grigorov <golantster@gmail.com>
2019-02-20 12:00:38 +0200

    update

--- scripts/50.configure_k8s_helm.sh
+++ scripts/50.configure_k8s_helm.sh
@@ -7,6 +7,8 @@ kubeadm init --pod-network-cidr=10.20.0.0/16
 mkdir -p /root/.kube
 cp -f /etc/kubernetes/admin.conf /root/.kube/config
 
+sleep 10
+
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 
 #curl -L https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml -o S{TEMPLATE_DIR}/calico.yaml
