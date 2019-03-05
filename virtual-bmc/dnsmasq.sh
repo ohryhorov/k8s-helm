d72087f40361c4f645bec945c5632f186692178b
Oleg Grigorov <golantster@gmail.com>
2019-03-05 10:50:22 +0200

    update script to check files

--- virtual-bmc/dnsmasq.sh
+++ virtual-bmc/dnsmasq.sh
@@ -1,1 +1,19 @@
-dnsmasq --conf-file=/etc/dnsmasq.d/dnsmasq-ironic.conf --log-dhcp -d
+#!/bin/bash
+
+CONF_FILE='/etc/dnsmasq.d/ironic-dnsmasq.conf'
+ADDHOSTS_FILE='/etc/dnsmasq.d/ironic.addnhosts'
+HOSTS_FILE='/etc/dnsmasq.d/ironic.hostsfile'
+
+if [ ! -f ${ADDHOSTS_FILE} ]; then
+    touch ${ADDHOSTS_FILE}
+fi
+
+if [ ! -f ${HOSTS_FILE} ]; then
+    touch ${HOSTS_FILE}
+fi
+
+if [ ! -f ${CONF_FILE} ]; then
+    cp /srv/k8s-helm/virtual-bmc/ironic-dnsmasq.conf ${CONF_FILE}
+fi
+
+dnsmasq --conf-file=${CONF_FILE} --log-dhcp -d
